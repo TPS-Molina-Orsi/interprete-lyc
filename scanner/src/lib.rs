343736f88efd770ed4f4a529892fadeacf1326bd
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-08-23 00:25:14 -0300

    feat: basic interpreter structure
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

---
+++ scanner/src/lib.rs
@@ -0,0 +1,18 @@
+struct Scanner {
+    field: i32,
+}
+
+pub fn scan(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
