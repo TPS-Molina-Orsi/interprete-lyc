0947baf75c652bc5d696c62e20da0c9dfe10063e
Taielmolina01 <tamolina@fi.uba.ar>
2025-08-23 12:08:36 -0300

    lazy template structure
    Co-authored-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# interprete-lyc
+# Scheme interpreter written in rust
+
+Scheme specification: https://standards.scheme.org/official/r7rs.pdf
 
 ## Propuestas nombres
 
--- scanner/src/lib.rs
+++ scanner/src/lib.rs
@@ -1,9 +1,91 @@
+use std::io::BufReader;
+use std::path::PathBuf;
+
 // struct Scanner {
 //     field: i32,
 // }
 
+/// Enum > Trait
+
+/// "Traits" - Interfaz
+
+/// All the tokens we know
+pub(crate) enum TokenType {
+    // (
+    LeftParens,
+    /// )
+    RightParens,
+    // +
+    Plus,
+    // -
+    Minus,
+    // *
+    Star,
+    // . // second best comment
+    Dot,
+    // / // best comment
+    ForwardSlash,
+    // \
+    BackSlash,
+    // [
+    LeftBracket,
+    // ]
+    RightBracket,
+    // '
+    Quote,
+    // `
+    BackQuote,
+    // `
+    Quasiquote,
+    // ,
+    Comma,
+    // #
+    Hashtag,
+    /// Identifer 
+    Identifier,
+    // 
+    String {
+        literal: String,
+    },
+    // 
+    Number {
+        literal: String,
+    },
+}
+
+
+enum Input {
+    Stdin,
+
+    File {
+        path: PathBuf,
+    }
+}
+
+struct Coordinate {
+    line: u32,
+    column: u32,
+}
+
+struct Location {
+    input: Input,
+    coord: Coordinate,
+}
+
+struct Token {
+    token_type: TokenType,
+
+    /// Lexeme that originated the Token
+    lexeme: String,
+
+    location: Location,
+}
+
 /// Function that handles scanning
-pub fn scan() {}
+/// This function will scan all the "lexemes" present in the passed in the input (either a file or stdin).
+pub fn scan<R>(input: BufReader<R>) -> Vec<Token> {
+    todo!()
+}
 
 #[cfg(test)]
 mod tests {
