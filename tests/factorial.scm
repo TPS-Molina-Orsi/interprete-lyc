09b3a5fbe66ede75d48047d7d33a2da8dc92055b
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-08-25 19:42:06 -0300

    chore: delete duplicated factorial.scm
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- tests/factorial.scm
+++
@@ -1,4 +0,0 @@
-(define (factorial n)
-  (if (= n 0)                      ; is the argument 0?
-      1                            ; then the answer is 1
-      (* n (factorial (- n 1)))))  ; otherwise, it's n * (n - 1)!
